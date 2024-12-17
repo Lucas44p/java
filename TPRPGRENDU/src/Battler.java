d0aa1d49fa5900f2e9add0871357ee4d8cb05d72
Lucas <lucasguilloteau33@gmail.com>
2024-12-17 15:31:20 +0100

    fc

--- TPRPGRENDU/src/Battler.java
+++ TPRPGRENDU/src/Battler.java
@@ -1,2 +1,34 @@
 public class Battler {
+    public String name;
+    public int hp;
+    public int power;
+
+
+    public Battler(String name, int hp, int power) {
+        this.name = name;
+        this.hp = hp;
+        this.power = power;
+    }
+
+    public void attack(Battler opponent) {
+        System.out.println(this.name + " attaque " + opponent.name + " avec une puissance de " + this.power);
+        opponent.looseHP(this.power);
+    }
+
+    public void looseHP(int amount) {
+        this.hp -= amount;
+        if (this.hp < 0)
+        System.out.println(this.name + " perd " + amount + " HP, il reste " + this.hp + " HP.");
+    }
+
+    public int getHp() {
+        return this.hp;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
+
+
+
--- TPRPGRENDU/src/Main.java
+++ TPRPGRENDU/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Battler playerBattler = new Battler("Equipe1", 100, 20);
+        Battler enemyBattler = new Battler("Equipe2", 80, 25);
+
+        Team playerTeam = new Team(playerBattler);
+        Team enemyTeam = new Team(enemyBattler);
+
+        Game game = new Game(playerTeam, enemyTeam);
+        game.play();
     }
 }
